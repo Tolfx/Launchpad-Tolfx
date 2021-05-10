bcc36960aacce3185960298260ac0e03a5134ca8
Tolfx <matteus12122@gmail.com>
2021-05-10 13:27:35 +0200

    Small changes

--- src/Handler/Connect.ts
+++ src/Handler/Connect.ts
@@ -4,7 +4,6 @@ export default function Connect(pad: Launchpad)
 {
     pad.connect().then(e => {
         console.log(e);
-        pad.reset(3);
         let btns = Launchpad.Buttons.All;
         pad.col( pad.off, btns );
     });
