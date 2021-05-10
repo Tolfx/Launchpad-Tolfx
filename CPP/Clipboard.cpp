03b3c59bbfd75d67c8464c5ab850d4fa12ef9248
Tolfx <matteus12122@gmail.com>
2021-05-10 13:15:25 +0200

    Small changes

--- CPP/Clipboard.cpp
+++ CPP/Clipboard.cpp
@@ -4,27 +4,21 @@
 
 std::string GetClipboardText()
 {
-    // Try opening the clipboard
     if (!OpenClipboard(nullptr))
         return (std::string)"";
 
-    // Get handle of clipboard object for ANSI text
     HANDLE hData = GetClipboardData(CF_TEXT);
     if (hData == nullptr)
         return (std::string)""; // error
 
-      // Lock the handle to get the actual text pointer
     char* pszText = static_cast<char*>(GlobalLock(hData));
     if (pszText == nullptr)
-        return (std::string)""; // error
+        return (std::string)"";
 
-      // Save text in a string class instance
     std::string text(pszText);
 
-    // Release the lock
     GlobalUnlock(hData);
 
-    // Release the clipboard
     CloseClipboard();
 
     return text;
@@ -33,9 +27,6 @@ std::string GetClipboardText()
 
 int main()
 {
-    std::string text = GetClipboardText();
-
-    std::cout << text << std::endl;
-
+    std::cout << GetClipboardText() << std::endl;
     return 0;
 }
